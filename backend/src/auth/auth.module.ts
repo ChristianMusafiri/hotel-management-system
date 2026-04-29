4a8d3e7eee3207fedc4ce6385368d552decc9a1c
Christian <christianmusafiri1@gmail.com>
2026-04-29 14:00:59 +0200

    feat: JWT authentification with role support added

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
+// import et ajout jwt et pass pour le token
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import 'dotenv/config';
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'SECRET_HOTEL_M_2026',
+      signOptions: { expiresIn: '24h' }
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,11 +3,53 @@ import { PrismaService } from '../prisma/prisma.service';// import Service
 import * as bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { JwtService } from '@nestjs/jwt'; //import related to token
+
+import { Prisma } from '@prisma/client'; //Prisma
+
+// type qui inclut les roles pour rassurer Typescript
+type UserWithRoles = Prisma.UserGetPayload<{
+    include: { roles: { include: { role: true } } }
+}>;
 
 @Injectable()
 export class AuthService {
     // NestJS injecte automatiquement PrismaService
-    constructor(private prisma: PrismaService) {}
+    constructor(
+        private prisma: PrismaService,
+        private jwtService: JwtService, //Injecte le service JWT
+    ) {}
+
+    // ajout related to token: JwtService
+    async login(loginDto: LoginDto) {
+        const user = await this.prisma.user.findUnique({
+            where: { username: loginDto.username },
+            include: { roles: { include: { role: true } } }
+        }) as UserWithRoles | null; // best way to make typescript in confidence that include is in the Prisma request
+
+        if (!user) throw new UnauthorizedException('Identifiants invalides');
+        const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+        if (!isPasswordValid) throw new UnauthorizedException('Identifiants invalides');
+        //
+        const userRoles = user.roles.map(ur => ur.role.name);
+
+        //donnees badge = payload
+        const payload = {
+            sub: user.id,
+            username: user.username,
+            roles: userRoles
+        };
+
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+            user: {
+                id: user.id,
+                username: user.username,
+                name: user.name,
+                roles: userRoles
+            },
+        };
+    } 
 
     //Methode pour enregistrer l'utilisateur 
     async register(registerDto: RegisterDto){
